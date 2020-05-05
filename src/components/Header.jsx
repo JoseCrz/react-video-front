da62ee02b4ffddc1eed110698fe4e62443f57354
Jose Cuevas <josecueram@gmail.com>
2020-05-04 20:49:14 -0500

    Created Header component structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="Header">
+        <figure className="Header__logo-container">
+            <img className="Header__logo" src="./img/logo-white.png" alt="Reactivid Logo" width="150px" />
+        </figure>
+        <nav className="Header__nav">
+            <div className="Header__nav-profile">
+                <img src="./img/user-icon.svg" alt="Profile Icon" width="50px" />
+                <p>Profile</p>
+            </div>
+            <ul className="Header__nav-menu">
+                <li><a href="#">Settings</a></li>
+                <li><a href="#">Sign out</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+import App from './containers/App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
