67ecd671274ce3e397df6f4b77ee3320a64f0a39
Jose Cuevas <josecueram@gmail.com>
2020-05-04 19:00:07 -0500

    installed and configured sass

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import '../assets/sass/App.scss'
+
 const HelloWorld = () => {
   return (
     <h1>Hello React World!</h1>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -24,6 +25,16 @@ module.exports = {
         use: {
           loader: 'html-loader'
         }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader',
+        ]
       }
     ]
   },
@@ -31,6 +42,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css'
     })
   ]
 }
