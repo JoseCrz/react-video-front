045cefb31f0ba13bf967063fcd0b83147dfe5c43
Jose Cuevas <josecueram@gmail.com>
2020-05-05 21:10:15 -0500

    created fake db

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 import '../assets/sass/components/Search.scss'
 const Search = () => {
   return (
-    <section class="Search">
-        <h2 class="Search__title">What are you going to watch?</h2>
-        <input class="Search__input" type="text" placeholder="Search..." />
+    <section className="Search">
+        <h2 className="Search__title">What are you going to watch?</h2>
+        <input className="Search__input" type="text" placeholder="Search..." />
     </section>
   )
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Header from '../components/Header'
 import Search from '../components/Search'
@@ -9,6 +9,17 @@ import Footer from '../components/Footer'
 import '../assets/sass/App.scss'
 
 const App = () => {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/initalState')
+    .then(response => response.json())
+    .then(data => {
+      setVideos(data)
+    })
+  }, [])
+
+  console.log(videos)
   return (
     <React.Fragment>
       <Header />
