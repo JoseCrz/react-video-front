245dbc6d9eadf3a3f2ccae2d2d2dc97309c435bc
Jose Cuevas <josecueram@gmail.com>
2020-05-06 18:10:12 -0500

    Added NotFound component & styles

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../assets/sass/components/NotFound.scss'
+
+const NotFound = () => {
+  return (
+    <section className="Not-found">
+        <p className="Not-found__emoji">☹️</p>
+        <h1 className="Not-found__title">Page not found</h1>
+    </section>
+  )
+}
+
+export default NotFound
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login}/>
       <Route exact path="/register" component={Register} />
+      <Route component={NotFound} />
     </Switch>
     </BrowserRouter>
   )
