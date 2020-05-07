b4941b2d16fa8e81d5ccebccf2bb578664ff5292
Jose Cuevas <josecueram@gmail.com>
2020-05-07 17:10:17 -0500

    added hook to Form component

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import twitterIcon from '../assets/static/twitter-icon.svg'
@@ -7,14 +7,33 @@ import googleIcon from '../assets/static/google-icon.svg'
 import '../assets/sass/components/Login.scss'
 
 const Login = () => {
+  const [form, setForm] = useState({
+    email:'',
+  })
+
+  const handleOnChange = event => {
+    event.preventDefault()
+
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+  const handleOnSubmit = event => {
+    event.preventDefault()
+    console.log(form)
+  }
+
   return (
     <section className="Login">
         <div className="Login__container">
             <h1 className="Login__title">Welcome</h1>
-            <form action="" className="Login__form">
-                <input className="Login__input" type="email" placeholder="User/Email" aria-label="Enter user or email" />
-                <input className="Login__input" type="password" placeholder="Password" aria-label="Enter password" />
-                <button className="Login__button" aria-label="Login in button">Sign in</button>
+            <form onSubmit={handleOnSubmit}  className="Login__form">
+                <input name="email" onChange={handleOnChange} className="Login__input" type="email" placeholder="User/Email" aria-label="Enter user or email" />
+                <input name="password" onChange={handleOnChange} className="Login__input" type="password" placeholder="Password" aria-label="Enter password"  />
+                <button className="Login__button" aria-label="Login in button" type="submit">Sign in</button>
                 <div className="Login__options">
                     <label className="Login__label" htmlFor="remember">
                         <input id="remember" type="checkbox" value="remember" name="remember" />Remember me
