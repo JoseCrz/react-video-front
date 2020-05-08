63ae2d446a07931501483e12ee13c3c5b067283c
Jose Cuevas <josecueram@gmail.com>
2020-05-08 16:08:36 -0500

    added more functionality to Player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import NotFound from '../containers/NotFound'
+import { Redirect } from 'react-router-dom'
+
+import { getVideoSource } from '../actions'
 
 import '../assets/sass/components/Player.scss'
 
 const Player = props => {
   const { id } = props.match.params
-  return (
-    <div className="Player">
-      <video className="Player__video" controls autoPlay muted>
-        <source  src="" type="video/mp4"/>
-      </video>
-      <div className="Player__button-container">
-        <button onClick={() => props.history.goBack()} className="Player__button" type="button">Back</button>
+  const hasPlaying = Object.keys(props.playing).length > 0
+
+  useEffect(() => {
+    props.getVideoSource(id)
+  }, [])
+
+  return hasPlaying
+    ? (
+      <div className="Player">
+        <video className="Player__video" controls autoPlay muted>
+          <source  src={props.playing.source} type="video/mp4"/>
+        </video>
+        <div className="Player__button-container">
+          <button onClick={() => props.history.goBack()} className="Player__button" type="button">Back</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+    : <NotFound />
+    // : <Redirect to="/404/" />
 }
 
-export default Player
+const mapStateToProps = state => {
+  return {
+    playing: state.playing
+  }
+}
+
+const mapDispatchToProps = {
+  getVideoSource,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player)
