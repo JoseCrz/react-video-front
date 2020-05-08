d0928cc6ddec8f8750dac143ef769e3d6ad12c86
Jose Cuevas <josecueram@gmail.com>
2020-05-08 14:21:50 -0500

    added funtionality to player to go back

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 import '../assets/sass/components/Player.scss'
 
-const Player = () => {
+const Player = props => {
+  const { id } = props.match.params
   return (
     <div className="Player">
       <video className="Player__video" controls autoPlay muted>
         <source  src="" type="video/mp4"/>
       </video>
       <div className="Player__button-container">
-        <button className="Player__button" type="button">Back</button>
+        <button onClick={() => props.history.goBack()} className="Player__button" type="button">Back</button>
       </div>
     </div>
   )
