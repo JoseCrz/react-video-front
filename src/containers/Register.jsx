ca774b613af3494fafad75b0ceb9803615df83c3
Jose Cuevas <josecueram@gmail.com>
2020-05-08 12:52:40 -0500

    added hooks to Register component

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,17 +1,36 @@
-import React  from 'react'
+import React, {useState}  from 'react'
 import { Link } from 'react-router-dom'
 
 import '../assets/sass/components/Login.scss'
 
 const Register = () => {
+  const [form, setForm] = useState({
+    email:'',
+    name: '',
+    password:''
+  })
+
+  const handleInput = event => {
+    setForm({
+      ...form,
+      [event.target.name] : event.target.value
+    })
+  }
+
+  const handleOnSubmit = event => {
+    event.preventDefault()
+
+    console.log(form)
+  }
+
   return (
     <section className="Login">
         <div className="Login__container Login__container--Login-up">
             <h1 className="Login__title">Don't have an account?</h1>
-            <form action="" className="Login__form Login__form--Login-up">
-                <input className="Login__input" type="text" placeholder="Username" aria-label="Enter a username" />
-                <input className="Login__input" type="email" placeholder="Email" aria-label="Enter an email" />
-                <input className="Login__input" type="password" placeholder="Password" aria-label="Enter a password" />
+            <form onSubmit={handleOnSubmit} className="Login__form Login__form--Login-up">
+                <input onChange={handleInput} name="name" className="Login__input" type="text" placeholder="Username" aria-label="Enter a username" />
+                <input onChange={handleInput} name="email" className="Login__input" type="email" placeholder="Email" aria-label="Enter an email" />
+                <input onChange={handleInput} name="password" className="Login__input" type="password" placeholder="Password" aria-label="Enter a password" />
                 <button className="Login__button" aria-label="Login up button">Login up</button>
             </form>
             <article className="Login__sign-up">
