451d4b107fb910281c54ee2bf1716d6fddb8ed19
Jose Cuevas <josecueram@gmail.com>
2020-05-18 14:48:00 -0500

    configured webpack for express

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import webpack from 'webpack'
+
 import config from '../config'
 
 const { DEV, PORT } = config
@@ -7,6 +9,16 @@ const app = express()
 
 if (DEV) {
   console.log('Working in development mode')
+
+  const webpackConfig = require('../../webpack.config')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const webpackCompiler = webpack(webpackConfig)
+  const webpackServerConfig = { port: PORT, hot: true }
+  
+  app.use(webpackDevMiddleware(webpackCompiler, webpackServerConfig))
+  app.use(webpackHotMiddleware(webpackCompiler))
+
 }
 
 app.get('*', (req, res) => {
