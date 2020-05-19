400ad9c3f70893d3812317a7af3e3efe9b4a6402
Jose Cuevas <josecueram@gmail.com>
2020-05-19 00:12:00 -0500

    started configuring webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { DEV } = require('./src/config')
+
+const entry = ['./src/frontend/index.js']
+let mode = 'production'
+
+if (DEV) {
+  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true')
+  mode = 'development'
+}
 
 module.exports = {
-  entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
-  mode: 'development',
+  entry,
+  mode,
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src/backend/public'),
     filename: 'assets/app.js',
     publicPath: '/',
   },
@@ -55,7 +64,7 @@ module.exports = {
     historyApiFallback: true
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    DEV ? new webpack.HotModuleReplacementPlugin() : () => {},
     new MiniCssExtractPlugin({
       filename: 'assets/app.css'
     })
