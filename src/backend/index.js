ecdcd65ea7440b7914a35b8a46b683b64f526da6
Jose Cuevas <josecueram@gmail.com>
2020-05-18 21:26:32 -0500

    added asset-require-hook to serve static files from server

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,4 +7,15 @@ require('@babel/register')({
   ]
 })
 
+// ! MAKE SURE TO BE USING file-loader 5.1.0, otherwise it won't work
+require('asset-require-hook')({
+  extensions: ['jpg', 'png', 'svg', 'gif'],
+  name: '/assets/[hash].[ext]',
+})
+
+// require('asset-require-hook')({
+//   extensions: ['jpg', 'png', 'gif'],
+//   name: '/assets/[hash].[ext]',
+// })
+
 require('./server')
