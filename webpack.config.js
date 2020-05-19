b491fa7791648bb91e8b15e7d2c74d638f2717d1
Jose Cuevas <josecueram@gmail.com>
2020-05-19 00:27:38 -0500

    removed html-loader & html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -32,12 +32,6 @@ module.exports = {
         }
       },
       {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader'
-        }
-      },
-      {
         test: /\.(s*)css$/,
         use: [
           {
