a340637f7d35a59ff8ce48ebbf73b1cfb0047a40
Jose Cuevas <josecueram@gmail.com>
2020-05-25 11:48:42 -0500

    updated config with new ENV Variables

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 const config = {
   DEV: process.env.NODE_ENV !== 'production',
-  PORT: process.env.PORT || 3000
+  PORT: process.env.PORT || 3000,
+  apiUrl: process.env.API_URL,
+  apiKeyToken: process.env.API_KEY_TOKEN,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
 
 module.exports = config
